39aea821349bb800f3f3ca761c3c16799a631ec3
Aniket <aniketwankar004@gmail.com>
2022-08-27 17:04:05 +0530

    add css

--- src/PortfolioCantainer/Home/Profile.js
+++ src/PortfolioCantainer/Home/Profile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Typical from "react-typical";
+import "./Profile.css";
 
 export default function Profile() {
   return (
@@ -8,21 +9,23 @@ export default function Profile() {
         <div className="profile-parent">
           <div className="profile-details">
             <div className="colz">
-              <a href="#">
-                <i className="fa fa-facebook-square"></i>
-              </a>
-              <a href="#">
-                <i className="fa fa-google-plus-square"></i>
-              </a>
-              <a href="#">
-                <i className="fa fa-instagram"></i>
-              </a>
-              <a href="#">
-                <i className="fa fa-youtube-square"></i>
-              </a>
-              <a href="#">
-                <i className="fa fa-twitter"></i>
-              </a>
+              <div className="colz-icon">
+                <a href="#">
+                  <i className="fa fa-facebook-square"></i>
+                </a>
+                <a href="#">
+                  <i className="fa fa-google-plus-square"></i>
+                </a>
+                <a href="#">
+                  <i className="fa fa-instagram"></i>
+                </a>
+                <a href="#">
+                  <i className="fa fa-youtube-square"></i>
+                </a>
+                <a href="#">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </div>
             </div>
 
             <div className="profile-details-name">
@@ -45,15 +48,31 @@ export default function Profile() {
                       1000,
                       "MERN Dev",
                       1000,
-                      "Cross Platform Dev",
+                      "Cross Platform Dev.",
                       100,
                       "React/React Native Dev",
                       1000,
                     ]}
                   />
                 </h1>
+                <span className="profile-role-tagline">
+                  knack of building applications with front and back end
+                  operations.
+                </span>
               </span>
             </div>
+            <div className="profile-options">
+              <button className="btn primary-btn">
+                {""}
+                Hire Me{" "}
+              </button>
+              <a href="#" download="Ehiedu ehizcv.pdf">
+                <button className="btn highlighted-btn">Get Resume</button>
+              </a>
+            </div>
+          </div>
+          <div className="profile-picture">
+            <div className="profile-picture-background"></div>
           </div>
         </div>
       </div>
